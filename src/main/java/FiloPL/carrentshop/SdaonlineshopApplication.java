619ad1b1827f13c2be183fec5517d4be09ebefb2
Tomasz Filo Zegarlicki <tzegarlicki@gmail.com>
2020-01-08 18:51:52 +0100

    add empty repo

---
+++ src/main/java/FiloPL/carrentshop/SdaonlineshopApplication.java
@@ -0,0 +1,13 @@
+package FiloPL.carrentshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaonlineshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdaonlineshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/FiloPL/carrentshop/SdaonlineshopApplicationTests.java
@@ -0,0 +1,13 @@
+package FiloPL.carrentshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdaonlineshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
